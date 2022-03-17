417d43a99e3976f6892f10db4e32f95921ee0581
Tia <mtvang@uncc.edu>
2022-03-17 16:10:53 -0400

    update

--- homechefapp/views.py
+++ homechefapp/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import requests
+
+# https://api.spoonacular.com/recipes/complexSearch?query=pasta&maxFat=25&number=2&apiKey=af8f73de917040b2b3b4e5b78fe4947a
 
 # Create your views here.
 # render method(1st param = request obj, 2nd param = template name (template path), 3rd param = {'key':'value'})
+
+ 
 
 def profile(request):
     return render(request, 'homechefapp/profile.html', {'title': 'Profile', 'name':'Harry Potter'})
@@ -17,4 +22,5 @@ def about(request):
     return render(request, 'homechefapp/about.html', {'title': 'About'})
 
 def home(request):
+    response = requests.get('')
     return render(request, 'homechefapp/home.html', {'title': 'Home'})

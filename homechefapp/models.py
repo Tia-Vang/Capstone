0de9f2a16f2157b18b3169545399e83e6750fa15
Chris Sarkisyan <csarkisy@uncc.edu>
2022-03-17 22:11:59 -0700

    added the bare-bones of a model for recipie

--- homechefapp/models.py
+++ homechefapp/models.py
@@ -8,3 +8,16 @@ class user(models.Model):
     email = models.CharField(max_length = 100)
     password = models.CharField(max_length = 100)
     username = models.CharField(max_length = 100)
+
+class recipe(models.Model):
+    #this class will hold each user's data
+    #so far, these feilds will each accept a sequence of charecters at most 100 in length
+    title = models.CharField(max_length = 100)
+    body = models.CharField(max_length = 1000000)
+    url = models.CharField(max_length = 10000000) #should hold a shareable url of the recipie
+    #add one for tags (keywords to search for) here
+    #add one for image here
+    #add one for ratings here
+    #add one for view blocks (f a user choses not to see this one)
+    #add one for reports
+    #add one for comments
